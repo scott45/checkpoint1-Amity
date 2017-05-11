8ebb6307be32e81a433ec6a58f9f9bcb00795625
scotty-b <businge.scott@andela.com>
2017-05-11 15:16:31 +0300

    modify person class to include getter methods

--- amity-app/people/person.py
+++ amity-app/people/person.py
@@ -1,11 +1,16 @@
 __author__ = 'scotty'
 
-class Person():
 
-    def __init__(self, f_name, l_name, person_lable):
+class Person(object):
+    def __init__(self, f_name, l_name, person_label):
         self.f_name = f_name.strip().title()
         self.l_name = l_name.strip().title()
-        self.person_lable = person_lable.strip().title()
+        self.person_lable = person_label.strip().title()
         self.accomodate = 'N'
 
-        
+    def get_all_names(self):
+        all_names = self.f_name + ' ' + self.l_name
+        self.all_names = all_names
+
+    def assign_qualifier(self, qualifier):
+        self.qualifier = qualifier
