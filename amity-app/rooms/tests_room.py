97c09018ac3cf3621b9d9e205da236f84e70f09f
scotty-b <businge.scott@andela.com>
2017-05-11 23:38:15 +0300

    refactor code to adhere to PEP 8 standards

--- amity-app/rooms/tests_room.py
+++ amity-app/rooms/tests_room.py
@@ -5,7 +5,6 @@ from .room import LivingSpace, Office
 
 
 class TestRoomClassFunctionality(unittest.TestCase):
-
     # tests that maximum for ls and O is 4 & 6
     def test_capacity(self):
         office = Office('red')
@@ -18,20 +17,19 @@ class TestRoomClassFunctionality(unittest.TestCase):
         ls = LivingSpace('redddishh')
         self.assertEqual(ls.add_person('one'), 3)
 
-    # O decrease by one when add person method is called
+    # Office decrease by one when add person method is called
     def test_capacity_reduces_by_one_for_office(self):
         of = Office('reddd')
         self.assertEqual(of.add_person('one'), 5)
 
+    # tests whether room and type are used correctly when passing arguments for office
     def test_office_is_instance_of_class_Office(self):
-        '''
-        This simply tests that the room type and room_name are passed
-        accordingly for a office. The second assert tests whether string
-        formatting actually occurred
-        '''
         office = Office('green')
         self.assertEqual('Office', office.room_type)
+        self.assertNotEqual('green', office.room_name)
 
+    # tests whether room and type are used correctly when passing arguments for livingspace
     def test_living_space_is_instance_of_class_LivingSpace(self):
         ls = LivingSpace('greenish')
         self.assertEqual('Living Space', ls.room_type)
+        self.assertNotEqual('greenish', ls.room_name)
