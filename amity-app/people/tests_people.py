fb2761607bd4363f9f4b3ec52d0ee15461a0583e
scotty-b <businge.scott@andela.com>
2017-05-11 22:28:27 +0300

    add testcase to check staff or fellow label

--- amity-app/people/tests_people.py
+++ amity-app/people/tests_people.py
@@ -8,3 +8,7 @@ class TestPersonClass(unittest.TestCase):
     def test_Staff_person_label(self):
         s = Staff("taracha", "rogers")
         self.assertEqual(s.person_lable, "Staff")
+
+    def test_Fellow_person_label(self):
+        f = Fellow("scott", "businge")
+        self.assertEqual(f.person_lable, "Fellow")
