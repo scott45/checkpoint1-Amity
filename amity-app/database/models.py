1375892fb633ac4e213b9fb603b40dfbff7f993d
scotty-b <businge.scott@andela.com>
2017-05-11 16:55:55 +0300

    model the persons table

--- amity-app/database/models.py
+++ amity-app/database/models.py
@@ -4,3 +4,19 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class People(Base):
+    __tablename__ = 'people'
+    id = Column(Integer, primary_key=True)
+    person_qualifier = Column(String(50), unique=True)
+    person_name = Column(String(80))
+    person_label = Column(String(25))
+    wants_accommodation = Column(String(5))
+    office_allocated = Column(String(50))
+    living_space_allocated = Column(String(50))
+
+    def __repr__(self):
+        return "<Person(person_name='%s')>" % self.person_name
