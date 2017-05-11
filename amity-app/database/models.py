c01d0bcb690e5c6617f8f7c4390f12dbaaa196e9
scotty-b <businge.scott@andela.com>
2017-05-11 16:46:24 +0300

    create models to hold the sqlalchemy database

---
+++ amity-app/database/models.py
@@ -0,0 +1,6 @@
+__author__ = 'scotty'
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
