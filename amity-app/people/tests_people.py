0e47e5b4cdb24461074c57b5a157ccdeee1dc06b
scotty-b <businge.scott@andela.com>
2017-05-11 23:11:06 +0300

    final commit testing person

--- amity-app/people/tests_people.py
+++ amity-app/people/tests_people.py
@@ -5,10 +5,13 @@ from .person import Staff, Fellow
 
 
 class TestPersonClass(unittest.TestCase):
+    def test_Fellow_two_names(self):
+        pass
+
     def test_Staff_person_label(self):
         s = Staff("taracha", "rogers")
-        self.assertEqual(s.person_lable, "Staff")
+        self.assertEqual(s.person_label, "Staff")
 
     def test_Fellow_person_label(self):
         f = Fellow("scott", "businge")
-        self.assertEqual(f.person_lable, "Fellow")
+        self.assertEqual(f.person_label, "Fellow")
