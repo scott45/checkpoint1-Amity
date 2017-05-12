bae45f2e149d123e6d630bdea8e9a8f3a22d3e03
scotty-b <businge.scott@andela.com>
2017-05-12 12:09:49 +0300

    minimal code (initialization) to make test pass

--- amity-app/amity.py
+++ amity-app/amity.py
@@ -2,4 +2,22 @@ __author__ = 'scotty'
 
 
 class Amity(object):
-    pass
+    def __init__(self):
+        self.offices = {
+            'available': [],
+            'unavailable': []
+        }
+        self.living_spaces = {
+            'available': [],
+            'unavailable': []
+        }
+        self.rooms = []
+        self.fellows = []
+        self.staff = []
+        self.people = self.staff + self.fellows
+        self.f_ids = [0]
+        self.s_ids = [0]
+        # # Regarding allocations
+        self.staff_allocations = []
+        self.fellow_allocations = []
+        self.unallocated_persons = []
