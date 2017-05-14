65acba96d66f5d8c1db30dd4d94d181c105fee42
scotty-b <businge.scott@andela.com>
2017-05-14 21:27:38 +0300

    add validate method with required checkers

--- amity-app/amity.py
+++ amity-app/amity.py
@@ -95,3 +95,59 @@ class Amity(object):
             file.write(output)
             click.secho('Printed to %s.txt' % filename, fg='green')
             return 'Print to file'
+
+    # validates the add person functionality
+    def validate_person(self, first_name, other_name, person_label,
+                        accommodate='N'):
+        if type(first_name) != str or type(other_name) != str:
+            click.secho('Incorrect data type input.', fg='white', bold=True)
+            return 'input-Error, name should be string'
+        if not first_name.isalpha() or not other_name.isalpha():
+            click.secho('Names must be in alphabetical strings',
+                        fg='white', bold=True)
+            return 'Non-Alphabetical names added'
+        if person_label.title() not in ['Fellow', 'Staff']:
+            click.secho('Please enter either Fellow or Staff for person type',
+                        fg='white', bold=True)
+            return 'Invalid Person Type'
+        if accommodate.upper() not in ['Y', 'N']:
+            click.secho('Please Enter Y or N for accommodation option.',
+                        fg='white', bold=True)
+            return 'Input-Error, accommodation option not Y or N'
+        accommodate = accommodate.upper()
+        person_label = person_label.title()
+        if person_label == 'Staff' and accommodate == 'Y':
+            accommodate = 'N'
+            click.secho(
+                'Staff not given accommodation. only an office is allocated.',
+                fg='white', bold=True)
+        all_names = first_name.title() + ' ' + other_name.title()
+        for person in self.people:
+            if person.full_name == all_names and \
+                    person.person_label == person_label.title():
+                click.secho('%s %s Already Exists.' % (person_label, all_names))
+                return 'This Person exists.'
+        if not self.offices['available'] and person_label == 'Staff':
+            click.secho(
+                'No offices to be allocated into currently.',
+                fg='red', bold=True)
+            return 'There are no offices created currently.'
+        if not self.living_spaces['available'] and not \
+                self.offices['available']:
+            click.secho(
+                'Not any room (office or living space) has been created in the system yet.',
+                fg='red', bold=True)
+            return 'There are no rooms in the system.'
+
+        if accommodate == 'Y' and person_label == 'Fellow':
+            if not self.living_spaces['available']:
+                output = 'Add a living space for this fellow '
+                output += 'to be allocated both.'
+                click.secho(output, fg='red', bold=True)
+                return 'No Living space for fellow to be allocated into.'
+            elif not self.offices['available']:
+                output = 'Add an office for this fellow '
+                output += 'to be allocated both .'
+                click.secho(output, fg='red', bold=True)
+                return 'No office for fellow to be allocated into.'
+        return [all_names, accommodate, person_label]
