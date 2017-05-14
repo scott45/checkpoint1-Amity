7c442e6b6e7ce30f93fe55b82bcc8ded76c5605f
scotty-b <businge.scott@andela.com>
2017-05-14 20:50:41 +0300

    add create room method with required checkers

--- amity-app/amity.py
+++ amity-app/amity.py
@@ -1,5 +1,17 @@
 __author__ = 'scotty'
 
+import os
+import time
+import click
+from random import randint
+
+from rooms.room import LivingSpace, Office
+from people.person import Fellow, Staff
+from database.models import People, Rooms, DatabaseManager, Base
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 
 class Amity(object):
     def __init__(self):
@@ -17,7 +29,36 @@ class Amity(object):
         self.people = self.staff + self.fellows
         self.f_ids = [0]
         self.s_ids = [0]
-        # # Regarding allocations
         self.staff_allocations = []
         self.fellow_allocations = []
         self.unallocated_persons = []
+
+    # instatiates rooms as instances of the amity class
+    def create_room(self, room_type, room_name):
+        if type(room_type) != str or room_type.upper() not in ['O', 'L']:
+            click.secho('invalid input, please enter O or L for a room type.',
+                        fg='white', bold=True)
+            return 'Error. Invalid room type input.'
+        room_type = room_type.strip().upper()
+        room_name = room_name.strip().title()
+        if room_type == 'O':
+            room_type = 'Office'
+        if room_type == 'L':
+            room_type = 'Living Space'
+        for room in self.rooms:
+            if room.room_name == room_name and \
+                    room.room_type == room_type:
+                click.secho('%s %s already exists, choose another name'
+                            % (room_type, room_name),
+                            fg='white', bold=True)
+                return 'Room already created.'
+        if room_type == 'Office':
+            room = Office(room_name)
+            self.offices['available'].append(room.room_name)
+        elif room_type == 'Living Space':
+            room = LivingSpace(room_name)
+            self.living_spaces['available'].append(room.room_name)
+        self.rooms.append(room)
+        click.secho('%s %s has been created.' %
+                    (room.room_type, room.room_name), bold=True, fg='green')
+        return 'Room %s created.' % room.room_name
