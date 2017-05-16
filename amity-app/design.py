71deb73550907952672efb847f541abd80471bd4
scotty-b <businge.scott@andela.com>
2017-05-16 15:58:17 +0300

    add file that runs my intro and exit app designing

--- amity-app/design.py
+++ amity-app/design.py
@@ -9,17 +9,17 @@ from pyfiglet import figlet_format
 
 def app_intro():
     click.secho('!' * 40, fg='yellow')
-    click.secho('-' * 40, fg='white')
+    click.secho('-' * 40, fg='red')
     init(strip=not sys.stdout.isatty())  # # strip colors if stdout is redirected
-    cprint(figlet_format('Amity room allocation', font='big'), 'red')
-    click.secho('-' * 40, fg='white')
+    cprint(figlet_format('Amity room allocation', font='big'), 'cyan')
+    click.secho('-' * 40, fg='red')
     click.secho('!' * 40, fg='yellow')
 
 
 def intro_msg():
     click.secho(
         """
-    """""""""""'Add people, rooms and have them automatically allocated'"""""""""
+    """""""""""'Add people, rooms and have automatic allocations'"""""""""
         """, bold=True, fg='yellow')
 
 
@@ -34,3 +34,14 @@ def intro_header():
     click.secho('' * 75)
     click.secho('' * 75)
     intro_msg()
+
+
+def exit_bar():
+    with click.progressbar(range(1000),
+                           label=click.secho(
+                               'THANKS FOR USING AMITY',
+                               blink=True, bold=True),
+                           fill_char=click.style('  ', bg='yellow')) as bar:
+        for i in bar:
+            pass
+
